eab14363cdb73a452fcf20dc25dd92118c938f99
Andrey Molchanovskiy <a.molchanovskiy@gmail.com>
2019-06-24 10:37:54 +0300

    added Podfile, implemented RealmManager

---
+++ Ping Pong Results/Managers/RealmManager.swift
@@ -0,0 +1,97 @@
+//
+//  RealmManager.swift
+//  Ping Pong Results
+//
+//  Created by Andrey Molchanovskiy on 6/24/19.
+//  Copyright © 2019 test. All rights reserved.
+//
+
+import UIKit
+import RealmSwift
+
+class RealmManager {
+
+	// MARK: - Variables
+	
+	static let shared = RealmManager()
+	var realm: Realm = try! Realm()
+	
+	// MARK: - Methods
+	
+	/**
+	Adds an unmanaged object to this Realm.
+	
+	- parameter object: The object to be added to this Realm.
+	- parameter update: What to do if an object with the same primary key alredy exists. Must be `.error` for objects
+	without a primary key. Default is .error .
+	*/
+	func save(object: Object, update: Realm.UpdatePolicy = .error) {
+		
+		try! realm.write({
+			if !object.isInvalidated {
+				realm.add(object, update: update)
+			}
+		})
+	}
+	
+	/**
+	Adds an unmanaged objects to this Realm.
+	
+	- parameter objects: The objects to be added to this Realm.
+	- parameter update: What to do if an objects with the same primary key alredy exists. Must be `.error` for objects
+	without a primary key. Default is .error .
+	*/
+	func save(objects: [Object], update: Realm.UpdatePolicy = .error) {
+		
+		try! realm.write({
+			realm.add(objects, update: update)
+		})
+	}
+	
+	/**
+	Returns all objects of the given type stored in the Realm.
+	
+	- parameter type: The type of the objects to be returned.
+	- returns: A `Results` containing the objects.
+	*/
+	func getObjectsWith(type: Object.Type) -> Results<Object>? {
+		return realm.objects(type)
+	}
+	
+	/**
+	Deletes an object from the Realm. Once the object is deleted it is considered invalidated.
+	
+	- parameter object: The object to be deleted.
+	*/
+	func remove(object: Object) {
+		try! realm.write({
+			if !object.isInvalidated {
+				realm.delete(object)
+			}
+		})
+	}
+	
+	/**
+	Deletes all objects of the given type stored to Realm. Once the objects is deleted it is considered invalidated.
+	
+	- parameter type: The object type to be deleted.
+	*/
+	func removeAll(for type: Object.Type) {
+		if let result = getObjectsWith(type: type) {
+			try! realm.write {
+				realm.delete(result)
+			}
+		}
+	}
+	
+	/**
+	Performs actions contained within the given block inside a write transaction.
+	
+	- parameter block: The block containing actions to perform.
+	*/
+	func write(callback: ()->Void) {
+		try? realm.write {
+			callback()
+		}
+	}
+}
---
+++ Podfile
@@ -0,0 +1,24 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '11.0'
+
+target 'Ping Pong Results' do
+    # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+    use_frameworks!
+    platform :ios, '11.0'
+		
+	# ignore all warnings from all pods
+	inhibit_all_warnings!
+		
+    # Kingfisher
+    # ==========
+    # Source:     https://github.com/onevcat/Kingfisher
+    # Purpose:    Used to display remote images from remote url in ListViewCells.
+    pod 'Kingfisher', '5.2.0'
+    
+    # RealmSwift
+    # ==========
+    # Source:     https://github.com/realm/realm-cocoa
+    # Purpose:    Used to store objects in local db, all our djambo items, and local djambo items, stored with realm.
+    pod 'RealmSwift', '3.16.2'
+    
+end
