c2f0b31bfd7e6d33dae2e2d3f53b1b607e89a109
Andrey Molchanovskiy <a.molchanovskiy@gmail.com>
2019-06-24 16:30:16 +0300

    - added no data label to favorites screen, updated delete favorites logic

--- Ping Pong Results/ViewControllers/Favourites View Controller/FavouritesTableViewController.swift
+++ Ping Pong Results/ViewControllers/Favourites View Controller/FavouritesTableViewController.swift
@@ -18,6 +18,43 @@ class FavouritesTableViewController: UITableViewController {
 		}
 	}
 	
+	lazy var noDataLabelView: UIView = {
+		let label = UILabel()
+		
+		// Enable Dynamic Type:
+		label.font = UIFont.preferredFont(forTextStyle: .callout)
+		label.adjustsFontForContentSizeCategory = true
+		
+		// Style label with white text:
+		label.textColor = .white
+		label.backgroundColor = .clear
+		label.text = "No favorites added yet!"
+		
+		// Style a grey background with rounded corners:
+		let noDataView = UIView()
+		noDataView.isHidden = true
+		noDataView.backgroundColor = UIColor.darkGray
+		noDataView.layer.cornerRadius = 4
+		noDataView.layer.masksToBounds = true
+		
+		// Set constraints of view around label:
+		noDataView.addSubview(label)
+		label.translatesAutoresizingMaskIntoConstraints = false
+		noDataView.addConstraint(NSLayoutConstraint(item: label, attribute: .leading, relatedBy: .equal, toItem: noDataView, attribute: .leading, multiplier: 1.0, constant: 12))
+		noDataView.addConstraint(NSLayoutConstraint(item: noDataView, attribute: .trailing, relatedBy: .equal, toItem: label, attribute: .trailing, multiplier: 1.0, constant: 12))
+		noDataView.addConstraint(NSLayoutConstraint(item: label, attribute: .top, relatedBy: .equal, toItem: noDataView, attribute: .top, multiplier: 1.0, constant: 6))
+		noDataView.addConstraint(NSLayoutConstraint(item: noDataView, attribute: .bottom, relatedBy: .equal, toItem: label, attribute: .bottom, multiplier: 1.0, constant: 6))
+		
+		// Set constraints to position view at top of tableview:
+		self.tableView.addSubview(noDataView)
+		noDataView.translatesAutoresizingMaskIntoConstraints = false
+		self.tableView.addConstraint(NSLayoutConstraint(item: noDataView, attribute: .centerX, relatedBy: .equal, toItem: self.tableView, attribute: .centerX, multiplier: 1.0, constant: 0))
+		self.tableView.addConstraint(NSLayoutConstraint(item: noDataView, attribute: .width, relatedBy: .lessThanOrEqual, toItem: self.tableView, attribute: .width, multiplier: 0.92, constant: 0))
+		self.tableView.addConstraint(NSLayoutConstraint(item: noDataView, attribute: .top, relatedBy: .equal, toItem: self.tableView, attribute: .top, multiplier: 1.0, constant: 40))
+		
+		return noDataView
+	}()
+	
 	// MARK: - View Controller life-cycle
 	
 	override func viewDidLoad() {
@@ -32,18 +69,36 @@ class FavouritesTableViewController: UITableViewController {
 	override func viewWillAppear(_ animated: Bool) {
 		super.viewWillAppear(animated)
 		games = AppManager.shared.getFavouriteGameObjects()
+		
+		checkIfDataExistAndShowEmptyViewIfNeeded()
+		checkEditingState()
 	}
 	
 	// MARK: - Actions
 	
+	func checkIfDataExistAndShowEmptyViewIfNeeded() {
+		if games.count == 0 {
+			noDataLabelView.isHidden = false
+		} else {
+			noDataLabelView.isHidden = true
+		}
+	}
+	
+	func checkEditingState() {
+		if tableView.isEditing == true {
+			navigationItem.rightBarButtonItem?.title = "Done"
+		} else {
+			navigationItem.rightBarButtonItem?.title = "Edit"
+		}
+	}
+	
 	@objc func showEditing(sender: UIBarButtonItem) {
 		if tableView.isEditing == true {
 			tableView.isEditing = false
-			navigationItem.rightBarButtonItem?.title = "Edit"
 		} else {
 			tableView.isEditing = true
-			navigationItem.rightBarButtonItem?.title = "Done"
 		}
+		checkEditingState()
 	}
 	
 	// MARK: - Table view data source
@@ -79,7 +134,6 @@ class FavouritesTableViewController: UITableViewController {
 		return true
 	}
 	
-	
 	override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
 		if editingStyle == .delete {
 			// Delete the row from the data source
@@ -88,6 +142,12 @@ class FavouritesTableViewController: UITableViewController {
 				game.favourite = false
 			}
 			games.remove(at: games.firstIndex(of: game)!)
+			
+			if games.count == 0 {
+				checkIfDataExistAndShowEmptyViewIfNeeded()
+				tableView.isEditing = false
+				checkEditingState()
+			}
 		}
 	}
 	
